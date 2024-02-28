64c459c13a2a98d42ca4e64c70f7f221a66904d7
AsanteLA <asante.laryea.akrong@gmail.com>
2024-02-27 19:06:32 -0700

    Add project files.

---
+++ Mission08_Team0208/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission08_Team0208.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
