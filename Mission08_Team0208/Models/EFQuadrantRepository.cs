518f219e5254a4d160ab8afc5e10722fd69ef84b
AsanteLA <asante.laryea.akrong@gmail.com>
2024-02-27 19:52:06 -0700

    Interface and Ef

---
+++ Mission08_Team0208/Models/EFQuadrantRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Mission08_Team0208.Models
+{
+    public class EFQuadrantRepository
+    {
+        //private  ----- _context
+        public EFQuadrantRepository() 
+        {
+
+        }
+
+
+    }
+}
