9f4d471c69df2ed326e8b8d6d835fae2441163e4
ccristil <ccristil@byu.edu>
2024-02-28 09:28:48 -0700

    added the models and Db

---
+++ Mission08_Team0208/Models/TimeManagementMatrixContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission08_Team0208.Models;
+
+public partial class TimeManagementMatrixContext : DbContext
+{
+    public TimeManagementMatrixContext()
+    {
+    }
+
+    public TimeManagementMatrixContext(DbContextOptions<TimeManagementMatrixContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Quadrant> Quadrants { get; set; }
+
+    public virtual DbSet<Task> Tasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlite("Data Source=TimeManagementMatrix.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.ToTable("Category");
+        });
+
+        modelBuilder.Entity<Quadrant>(entity =>
+        {
+            entity.Property(e => e.QuadrantId).ValueGeneratedNever();
+        });
+
+        modelBuilder.Entity<Task>(entity =>
+        {
+            entity.Property(e => e.Task1).HasColumnName("Task");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
